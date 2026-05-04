7d1e95bb71a3427e1521ebc1134d99d9b8d41b16
saffaaumairaah <umairahsaffaa07@gmail.com>
2026-05-04 22:11:36 +0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+let eventDate = new Date("August 25, 2026 00:00:00").getTime();
+
+let timer = setInterval(function() {
+    let now = new Date().getTime();
+    let distance = eventDate - now;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
+
+document.getElementById("timer").innerHTML =
+days + " hari " + hours + " jam lagi"; 
+    document.getElementById("timer").innerHTML = days + " hari lagi";
+
+}, 1000);
+
+
+// COUNTER ANIMATION
+let count = 0;
+let target = 150;
+
+let counter = setInterval(function(){
+    if(count < target){
+        count++;
+        document.getElementById("counter").innerHTML = count;
+    }
+}, 20);
